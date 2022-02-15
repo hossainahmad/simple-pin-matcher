a14aeb38b6c9f17d10a69fcd65d769568bde5b8e
hossainahmad <hossain.ahmad38@gmail.com>
2022-02-15 17:10:07 +0600

    typed button added with event bubble handler

--- js/app.js
+++ js/app.js
@@ -12,3 +12,18 @@ function generatePin(){
     const pin = getPin();
     document.getElementById('display-input').value = pin;
 }
+
+document.getElementById('calc-key-pad').addEventListener('click', function(event){
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('typed-numbers-display');
+    if(isNaN(number)){
+        if(number == 'C'){
+            calcInput.value = '';
+        }
+    }
+    else{
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + number;
+        calcInput.value = newNumber;
+    }
+});
